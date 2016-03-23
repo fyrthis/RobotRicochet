1da7b2ef02ecddb2bd4453dbc898525b0b5ee99f
tom-ish <tomohiro.ishiwata@gmail.com>
2016-03-23 20:40:26 +0100

    commit missng java files

---
+++ src/model/State.java
@@ -0,0 +1,15 @@
+package model;
+
+import utils.Phase;
+
+public class State {
+	
+	static int tour;
+	static Phase phase;
+	
+	public State(){
+	}
+	
+	public void setTour(int t){ State.tour = t; }
+
+}
---
+++ src/utils/Phase.java
@@ -0,0 +1,5 @@
+package utils;
+
+public enum Phase {
+	INITIALISATION, REFLEXION, ENCHERE, RESOLUTION
+}

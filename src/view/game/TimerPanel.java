958f7ad100186b2cfd776c8a49852098d2f1677d
fyrthis <tanguy.retail@caramail.com>
2016-03-25 22:54:44 +0100

    java maj players

--- src/view/game/TimerPanel.java
+++ src/view/game/TimerPanel.java
@@ -1,23 +1,33 @@
 package view.game;
-import java.awt.Color;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.Timer;
 
 import model.Model;
 
 public class TimerPanel extends JPanel {
 	private static final long serialVersionUID = -650268772194032577L;
-	private JTextField timer;
+	//private JTextField timerField;
 	Model model;
 
-	public TimerPanel(Model model) {
+
+	/**
+	 * 
+	 */
+
+	private Timer timer;
+	private JTextField title = new JTextField("Timer : ");;
+	private JTextField time;
+
+	public TimerPanel(Model model){
 		super();
 		this.model = model;
+		/*
 		setBackground(Color.blue);
-		
+
 		setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.BOTH;
@@ -25,11 +35,29 @@ public class TimerPanel extends JPanel {
 		c.gridy = 0;
 		c.weightx = 1.0;
 		c.weighty = 1.0;
-		
-		timer = new JTextField("30");
-		timer.setEditable(false);
-		timer.setHorizontalAlignment(JTextField.CENTER);
-		add(timer, c);
+
+		timerField = new JTextField("30");
+		timerField.setEditable(false);
+		timerField.setHorizontalAlignment(JTextField.CENTER);
+		add(timerField, c);
+		 */
+		this.time = new JTextField("0");
+
+		int delay = 1000; //milliseconds
+		ActionListener taskPerformer = new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				//...Perform a task...
+				update();
+			}
+		};
+		timer = new Timer(delay, taskPerformer);
+		timer.start();
+	}
+
+	public void update(){
+		Integer currentTime = Integer.getInteger(time.getText());
+		currentTime++;
+		time.setText(currentTime.toString());
 	}
 
 }

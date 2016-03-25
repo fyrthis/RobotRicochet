a6cb15cdc90582fb1a652d112670596c3dd0b2ce
fyrthis <tanguy.retail@caramail.com>
2016-03-25 15:17:50 +0100

    add game state

---
+++ src/model/GameState.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.util.Observable;
+
+import utils.Phase;
+
+public class GameState extends Observable {
+	
+	static int tour;
+	static int currentSolution;
+	static Phase phase;
+	
+	public GameState(){
+	}
+	
+	public void setTour(int t){ GameState.tour = t; }
+	public void setCurrentSolution(int s){ GameState.currentSolution = s; }
+	public void setPhase(Phase p){
+		System.out.println("\t setting phase to : "+p);
+		GameState.phase = p;
+		this.setChanged();
+		this.notifyObservers(phase);
+	}
+
+}

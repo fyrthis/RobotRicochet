530cb77b7349165dfcb712d93ab2239e7db26264
fyrthis <tanguy.retail@caramail.com>
2016-03-30 20:08:14 +0200

    handle SIGINT, and enable admin speaking

--- C/client.h
+++ C/client.h
@@ -28,6 +28,9 @@ extern client_t * last_client;// = NULL;
 extern int nbClients;// = 0;
 extern int nbClientsConnecte;// = 0;
 
+extern pthread_cond_t  cond_at_least_2_players;
+
+
 void addClient(int socket, char *name, pthread_mutex_t* p_mutex);
 void rmClient(int socket, pthread_mutex_t* p_mutex);
 void disconnectClient(char *username, pthread_mutex_t* p_mutex);

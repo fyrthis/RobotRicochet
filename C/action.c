d699340b0aca83080819f9de66d2d08957b4d2a6
tom-ish <tomohiro.ishiwata@gmail.com>
2016-04-05 16:09:41 +0200

    delete all prints in Client part

--- C/action.c
+++ C/action.c
@@ -7,9 +7,15 @@
 
 
 int sendMessageAll(char *msg, pthread_mutex_t* p_mutex) {
-    pthread_mutex_lock(p_mutex);
+    puts("debug 1\n");
+    if(pthread_mutex_lock(p_mutex) != 0){
+    	perror("(Server:action.c:sendMessageAll) : Error on sendMessageAll, cannot lock the p_mutex\n");
+    }
+    puts("si ce debug echoue, :'(\n");
     if(clients==NULL) {
         printf("(Server:action.c:sendMessageAll) : Aucun client. Should never happened\n");
+    //} else if(clients) { TODO : Si qu'un client
+    //    printf("(sendMessageAll)Un seul client. Should never happened\n");
     } else {
         client_t * client = clients;
         while(client != NULL) {
@@ -17,17 +23,25 @@ int sendMessageAll(char *msg, pthread_mutex_t* p_mutex) {
                 if(write(client->socket,msg,(strlen(msg)*sizeof(char))) < 0){
                     perror("(Server:action.c:sendMessageAll) : Erreur ,cannot write on socket\n");
                 }
+                else {
+                    fprintf(stderr, "(Server:action.c:sendMessageAll) : Message send to all : %s\n", msg);
+                }
             }
             client = client->next;
         }
     }
-    pthread_mutex_unlock(p_mutex);
+    if(pthread_mutex_unlock(p_mutex) != 0){
+    	perror("(Server:action.c:sendMessageAll) : Error on sendMessageAll, cannot unlock the p_mutex\n");
+    }
 
     return 0;
 }
 
 int sendMessageAllExceptOne(char *msg, char *name, pthread_mutex_t* p_mutex) { //Except client with this name
-    pthread_mutex_lock(p_mutex);
+    if(pthread_mutex_lock(p_mutex) < 0){
+    	perror("(Server:action.c:sendMessageAllExceptOne) : Error on sendMessageAllExceptOne, cannot lock the p_mutex\n");
+    }
+    fprintf(stderr, "\ttest : %s\n", msg);
     
     if(clients==NULL) {
         printf("(Server:action.c:sendMessageAllExceptOne) : Aucun client. Should never happened\n");
@@ -38,11 +52,16 @@ int sendMessageAllExceptOne(char *msg, char *name, pthread_mutex_t* p_mutex) { /
                 if(write(client->socket,msg,(strlen(msg)*sizeof(char))) < 0){
                     perror("(Server:action.c:sendMessageAllExceptOne) : Erreur in tuAsTrouve, cannot write on socket\n");
                 }
+                else {
+                    fprintf(stderr, "(Server:action.c:sendMessageAllExceptOne) : Message send to %s : %s\n", client->name, msg);
+                }
             }
             client = client->next;
         }
     }
-    pthread_mutex_unlock(p_mutex);
+    if(pthread_mutex_unlock(p_mutex) < 0){
+    	perror("(Server:action.c:sendMessageAllExceptOne) : cannot unlock the p_mutex\n");
+    }
 
     return 0;
 }
@@ -64,6 +83,7 @@ int send_bienvenue(char *username, int socket) {
 	//S -> C : BIENVENUE/user/
     char *msg = (char*)calloc(13+strlen(username), sizeof(char));
     sprintf(msg, "BIENVENUE/%s/\n", username);
+    printf("%s", msg);
     if(write(socket,msg,strlen(msg)*sizeof(char)) < 0){
         perror("(Server:action.c:bienvenue) :  cannot write on socket\n");
     }
@@ -77,7 +97,8 @@ int send_bienvenue(char *username, int socket) {
 // S -> C : CONNECTE/user/
 int send_connexion(char *username, int socket) {
     char *msg = (char*)calloc(12+strlen(username), sizeof(char));
-    sprintf(msg, "CONNECTE/%s/\n", username);;
+    sprintf(msg, "CONNECTE/%s/\n", username);
+    printf("%s", msg);
     sendMessageAllExceptOne(msg, username, &client_mutex);
 
     return 0;
@@ -107,9 +128,13 @@ int sendGrid(char *gridStr, int socket) {
    /*Here is the mistake : do not put a char** into a strcat !!*/
     char *msg = (char*)calloc(4096, sizeof(char));
     
-    sprintf(msg, "SESSION/%s/\n", gridStr););
+    sprintf(msg, "SESSION/%s/\n", gridStr);
+    printf("%s", msg);
     if(write(socket,msg,strlen(msg)*sizeof(char)) < 0){
     	perror("(Server:action.c:sendGrid) : Erreur in sendGrid, cannot write on socket\n");
+    }
+    else {
+        fprintf(stderr, "(Server:action.c:sendGrid) : Grid send!\n");
     }
 
     return 0;
@@ -119,6 +144,7 @@ int sendGrid(char *gridStr, int socket) {
 int send_vainqueur(){
     char *msg = (char*)calloc(strlen(bilan)+13, sizeof(char));
     sprintf(msg, "VAINQUEUR/%s/\n", bilan);
+    printf("bilan : %s", msg);
     sendMessageAll(msg, &client_mutex);
     return 0;
 }
@@ -133,8 +159,12 @@ int send_vainqueur(){
 
 // S -> C : TOUR/enigme/bilan
 int sendEnigmaBilan(char *enigma, char *bilan) {
+    fprintf(stderr, "(Server:action.c:sendEnigmaBilan) : Enigma send!\n");
     char *msg = (char*)calloc(strlen(enigma)+strlen(bilan)+9, sizeof(char));
+    
     sprintf(msg, "TOUR/%s/%s/\n", enigma, bilan);
+
+    printf("%s", msg);
     sendMessageAll(msg, &client_mutex);
 
     return 0;
@@ -145,8 +175,12 @@ int sendEnigmaBilan(char *enigma, char *bilan) {
 int tuAsTrouve(int socket) {
 	char *msgActivePlayer = (char*)calloc(13, sizeof(char));
     sprintf(msgActivePlayer, "TUASTROUVE/\n");
+    fprintf(stderr, "%s\n", msgActivePlayer);
     if(write(socket,msgActivePlayer,(strlen(msgActivePlayer))*sizeof(char)) < 0){
     	perror("(Server:action.c:tuAsTrouve) : Erreur in tuAsTrouve, cannot write on socket\n");
+    }
+    else {
+        fprintf(stderr, "(Server:action.c:tuAsTrouve) : %s\n", msgActivePlayer);
     }
     return 0;
 }
@@ -157,6 +191,8 @@ int ilATrouve(char *activePlayer, int solution, int socket) {
     int currentSolutionLength = getIntLength(solution);
     char *msgOtherPlayers = (char*)calloc(15+strlen(activePlayer)+currentSolutionLength, sizeof(char));
     sprintf(msgOtherPlayers, "ILATROUVE/%s/%d/\n", activePlayer, solution);
+   
+    fprintf(stderr, "(Server:action.c:ilATrouve) : %s\n", msgOtherPlayers);
     
 	sendMessageAllExceptOne(msgOtherPlayers, activePlayer, &client_mutex);
     return 0;
@@ -177,8 +213,12 @@ int send_finReflexion() {
 int send_validation(int socket) {
     char *msg = (char*)calloc(13, sizeof(char));
     sprintf(msg, "VALIDATION/\n");
+    fprintf(stderr, "(Server:action.c:validation) : %s\n", msg);
     if(write(socket,msg,(strlen(msg))*sizeof(char)) < 0){
         perror("(Server:action.c:validation) : Erreur in validation(), cannot write on socket\n");
+    }
+    else {
+        fprintf(stderr, "(Server:action.c:validation) : %s\n", msg);
     }
     return 0;
 }
@@ -187,8 +227,12 @@ int send_validation(int socket) {
 int send_echec(char *username, int socket) {
 	char *msg = (char*)calloc(9 + strlen(username), sizeof(char));
     sprintf(msg, "ECHEC/%s/\n", username);
+    fprintf(stderr, "(Server:action.c:echec) : %s\n", msg);
     if(write(socket,msg,(strlen(msg))*sizeof(char)) < 0){
         perror("(Server:action.c:echec) : Erreur in echec(), cannot write on socket\n");
+    }
+    else {
+        fprintf(stderr, "(Server:action.c:echec) : %s\n", msg);
     }
     return 0;
 }
@@ -198,6 +242,7 @@ int send_nouvelleEnchere(char *username, int nbCoups) {
     int nbCoupsLength = getIntLength(nbCoups);
 	char *msg = (char*)calloc(20 + strlen(username) + nbCoupsLength, sizeof(char));
     sprintf(msg, "NOUVELLEENCHERE/%s/%d/\n", username, nbCoups);
+    fprintf(stderr, "(Server:action.c:nouvelleEnchere) : %s\n", msg);
     sendMessageAllExceptOne(msg, username, &client_mutex);
     return 0;
 }
@@ -207,6 +252,7 @@ int send_finEnchere(char *username, int nbCoups) {
     int nbCoupsLength = getIntLength(nbCoups);
     char *msg = (char*)calloc(15 + strlen(username) + nbCoupsLength, sizeof(char));
     sprintf(msg, "FINENCHERE/%s/%d/\n", username, nbCoups);
+    fprintf(stderr, "(Server:action.c:finEnchere) : %s\n", msg);
     sendMessageAll(msg, &client_mutex);
     return 0;
 }
@@ -219,6 +265,7 @@ int send_finEnchere(char *username, int nbCoups) {
 int solutionActive(char *username, char *deplacements) {
 	char *msg = (char*)calloc(15 + strlen(username) + strlen(deplacements), sizeof(char));
     sprintf(msg, "SASOLUTION/%s/%s/\n", username, deplacements);
+    fprintf(stderr, "(Server:action.c:solutionActive) : %s\n", msg);
     sendMessageAllExceptOne(msg, username, &client_mutex);
     return 0;
 }
@@ -227,6 +274,7 @@ int solutionActive(char *username, char *deplacements) {
 int send_bonneSolution() {
 	char *msg = (char*)calloc(8, sizeof(char));
     sprintf(msg, "BONNE/\n");
+    fprintf(stderr, "(Server:action.c:bonneSolution) : %s\n", msg);
     sendMessageAll(msg, &client_mutex);
     return 0;
 }
@@ -235,6 +283,7 @@ int send_bonneSolution() {
 int send_mauvaiseSolution(char *username) {
 	char *msg = (char*)calloc(12 + strlen(username), sizeof(char));
     sprintf(msg, "MAUVAISE/%s/\n", username);
+    fprintf(stderr, "(Server:action.c:mauvaiseSolution) : %s\n", msg);
     sendMessageAll(msg, &client_mutex);
     return 0;
 }
@@ -243,6 +292,7 @@ int send_mauvaiseSolution(char *username) {
 int send_finReso() {
 	char *msg = (char*)calloc(10, sizeof(char));
     sprintf(msg, "FINRESO/\n");
+    fprintf(stderr, "(Server:action.c:finResolution) : %s\n", msg);
     sendMessageAll(msg, &client_mutex);
     return 0;
 }
@@ -251,6 +301,7 @@ int send_finReso() {
 int send_tropLong(char *username) {
 	char *msg = (char*)calloc(12 + strlen(username), sizeof(char));
     sprintf(msg, "TROPLONG/%s/\n", username);
+    fprintf(stderr, "(Server:action.c:tropLong) : %s\n", msg);
     sendMessageAll(msg, &client_mutex);
     return 0;
 }
@@ -259,6 +310,7 @@ int send_tropLong(char *username) {
 int envoyerMessageAuxAutres(char *user,  char *message, int socket) {
     char *msg = (char*)calloc(strlen(user) + strlen(message) + 11 + 1, sizeof(char));
     sprintf(msg, "MESSAGE/%s/%s/\n", user, message);
+    fprintf(stderr, "(Server:action.c:envoyerMessageAuxAutres) : %s\n", msg);
     sendMessageAllExceptOne(msg, user, &client_mutex);
     return 0;
 }
--- src/controller/DebutSession.java
+++ src/controller/DebutSession.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.util.Observable;
 
+import players.Players.LocalPlayer;
 import utils.Phase;
 import model.Model;
 
@@ -29,6 +30,18 @@ class DebutSession extends Observable {
 	//(S -> C) Fin de la session courante, scores finaux de la session
 	void vainqueur(String bilan) {
 		model.setBilanCurrentSession(bilan);
+		
+		// On peut considérer que le vainqueur de la derniere partie est le vainqueur de la session
+		// puisqu'il faut atteindre un score objectif pour arrêter la session
+		String winner = model.getGameState().getActivePlayer();
+		if(winner.equals(LocalPlayer.getInstance().getName())){
+			// On peut afficher un JDialog au gagnant pour lui signifier sa victoire
+			
+		}
+		else {
+			// On peut aussi afficher un JDialog aux perdants pour leur signier leur défaite
+			
+		}
 	}
 
 }
